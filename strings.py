293629e941e908a7d50a5216864edd15e3c0decc
chrisNxtDoor <khreesnwandu@gmail.com>
2023-01-26 13:53:31 +0100

    added py file

---
+++ strings.py
@@ -0,0 +1,13 @@
+firstValue = "imagination"
+secondValue = "conceptualization"
+thirdValue = "consciousness"
+fourthValue = "intelligence"
+
+print(len(firstValue))
+print(len(secondValue))
+print(len(thirdValue))
+print(len(fourthValue))
+
+print(len(fourthValue[2:6]))
+print(len(secondValue[5:0]))
+print(len(fourthValue[4:7]))
